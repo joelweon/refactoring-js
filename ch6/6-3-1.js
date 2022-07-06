f7b913a5694d8da2fb48626d731cd53f1033bcc7
joelweon@gmail.com <79552dyf!ge>
2022-07-06 15:44:38 +0900

    변수 추출하기(6.3.1) - 최종 수정 -> 의미가 통한다면 간단한 변수명도 OK - 가격(price) = 기본가격 - 수량할인 + 배송비 - 수량할인: quantityDiscount vs discount - 배송비: shippingCost vs shipping

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,9 +1,10 @@
 // 표현식을 잘게 쪼개 의미있는 변수로 추출
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
   const basePrice = order.quantity * order.itemPrice;
-  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  const shipFee = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  // quantityDiscount vs discount
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  // shippingCost vs shipping
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
 
   return basePrice - quantityDiscount + shipFee;
 }
