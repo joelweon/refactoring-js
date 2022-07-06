959f154356faac090933ae1a5258c89e8dab547e
joelweon@gmail.com <79552dyf!ge>
2022-07-06 15:40:32 +0900

    변수 추출하기(6.3.1) - 1차 수정

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,9 @@
+// 표현식을 잘게 쪼개 의미있는 변수로 추출
 export function price(order) {
   // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipFee = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return basePrice - quantityDiscount + shipFee;
 }
