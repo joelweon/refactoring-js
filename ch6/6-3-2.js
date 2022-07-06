47bd0bfe3e6fc75597d7c162342760cdb870d21b
joelweon@gmail.com <79552dyf!ge>
2022-07-06 15:49:38 +0900

    변수 추출하기(6.3.2) - 1차 수정

--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -3,18 +3,12 @@ export class Order {
     this._data = aRecord;
   }
 
-  get quantity() {
-    return this._data.quantity;
-  }
-  get itemPrice() {
-    return this._data.itemPrice;
-  }
-
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    const data = this._data;
+    const basePrice = data.quantity * data.itemPrice;
+    const discount = Math.max(0, data.quantity - 500) * data.itemPrice * 0.05;
+    const shipping = Math.min(basePrice * 0.1, 100);
+
+    return basePrice - discount + shipping;
   }
 }
