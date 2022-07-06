bc260a6b26041a0499cb1cbaf2d2d25ad02ca120
joelweon@gmail.com <79552dyf!ge>
2022-07-06 14:28:36 +0900

    함수 추출(6.1) - 1차 수정

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,37 @@
 export function printOwing(invoice) {
+
+  printBanner();
+  calculateOutstanding();
+  recordDueDate();
+  printDetail();
+
   let outstanding = 0;
-
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
+  function calculateOutstanding() {
+    for (const o of invoice.orders) {
+      outstanding += o.amount;
+    }
   }
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  function recordDueDate() {
+    const today = new Date();
+    invoice.dueDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
+    );
+  }
 
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  function printBanner() {
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
+  }
+
+  function printDetail() {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  }
 }
 
 const invoice = {
