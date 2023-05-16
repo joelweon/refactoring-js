1b87663f14348cb86aea36fdffbc704f910b328e
joel <joel@nine2021.com>
2023-05-16 15:56:12 +0900

    매개변수 객체 만들기 (6.8) - 객체로 만들어 리팩토링

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,24 +1,31 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+export function readingsOutsideRange(readings, range) {
+  return readings.filter((r) => !range.contains(r.temp));
+}
+
+export class NumberRange {
+  #min;
+  #max;
+
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+
+  contains(num) {
+    return this.#min <= num && this.#max >= num;
+  }
 }
 
 const station = {
   name: 'ZB1',
   readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
+    {temp: 47, time: '2016-11-10 09:10'},
+    {temp: 53, time: '2016-11-10 09:20'},
+    {temp: 58, time: '2016-11-10 09:30'},
+    {temp: 53, time: '2016-11-10 09:40'},
+    {temp: 51, time: '2016-11-10 09:50'},
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+const operationPlan = new NumberRange(51, 53);
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+console.log(readingsOutsideRange(station.readings, operationPlan))
